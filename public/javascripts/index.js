6ed0af5d2cd01e78b527bfaa24464c07661d8f87
hguan6 <hguan6@asu.edu>
2016-07-27 14:34:38 -0700

    2nd Tag template

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,22 @@
-angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
-angular.module('ui.bootstrap.demo').controller('CarouselCtrl', function ($scope) {
+var mainApp = angular.module('ngApp', ['ngAnimate', 'ui.bootstrap','ngMap']);
+mainApp.factory('FoodService', function(){
+  var foodService = {
+    food: [
+    {imageLink:"images/Sichuan.jpg",name:"LaoSiChuan",address:"Chinese Cultural Center, 668 N 44th St #108, Phoenix, AZ 85008"},
+    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
+    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
+    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
+    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
+    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
+    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
+    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"}
+  ],
+    restaurant: {}
+  };
+  return foodService;
+});
+
+mainApp.controller('CarouselCtrl', function ($scope) {  
   $scope.myInterval = 5000;
   $scope.noWrapSlides = false;
   $scope.active = 0;
@@ -24,7 +41,8 @@ angular.module('ui.bootstrap.demo').controller('CarouselCtrl', function ($scope)
   ];   
 });
 
-angular.module('ui.bootstrap.demo').controller('TabsCtrl', function ($scope, $window){
+mainApp.controller('TabsCtrl',function ($scope, $timeout, NgMap, FoodService){
+  $scope.food = FoodService.food;
   $scope.rate = 7;
   $scope.max = 10;
   $scope.isReadonly = false;
@@ -34,20 +52,28 @@ angular.module('ui.bootstrap.demo').controller('TabsCtrl', function ($scope, $wi
     $scope.percent = 100 * (value / $scope.max);
   };
   
-    $scope.foods = [
-    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
-    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
-    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
-    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
-    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
-    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
-    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
-    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"}
-  ]
+  
+
+   $scope.maps = {};  
+
+        NgMap.getMap({ id: 'map1' }).then(function (map) {
+                map.showInfoWindow('bar1', 'marker1'); //show marker on map load
+                $scope.maps['map1'] = map;
+        });
 
   $scope.myVar = true;
-  $scope.myVarFunc = function(){
-	  $scope.myVar = !$scope.myVar;
-  }
- 
+  $scope.myVarFunc = function(food){
+	  $scope.myVar = !$scope.myVar;  
+    $scope.address = food.address;
+    $scope.restaurantName = food.name;
+    $timeout(function () {
+                        angular.forEach($scope.maps, function (map) {
+                                google.maps.event.trigger(map, 'resize');
+                        });
+                });
+  };
+
+
+     
+  
 });
