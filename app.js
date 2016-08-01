b90f950f4df403b16e626df665a3c03ab84b6e61
youhouhou <zehongma@gmail.com>
2016-08-01 15:21:17 -0700

    minor adjust

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var path = require("path");
 var app = express();
-<<<<<<< HEAD
-var port = process.env.PORT || 3300;
-||||||| merged common ancestors
 
-=======
-var port = process.env.PORT || 3000;
->>>>>>> upstream/master
+var port = process.env.PORT || 3300;
+
 
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
@@ -15,15 +11,10 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use("/static", express.static(path.join(__dirname, '/public/stylesheets')));
 app.use("/static", express.static(path.join(__dirname, '/public/javascripts')));
 app.use("/static", express.static(path.join(__dirname, '/public/images')));
-<<<<<<< HEAD
 
-||||||| merged common ancestors
-
-
-=======
 app.use("/static", express.static(path.join(__dirname, '/public/html')));
 
->>>>>>> upstream/master
+
 app.get('/', function(req, res) {
 	res.render('index.html');
 });
