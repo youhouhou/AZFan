01963aaed40a5afc3b4be68480044d8aa101fce6
hguan6 <hguan6@asu.edu>
2016-08-01 14:08:52 -0700

    Add some contents
    food and restaurants information is needed now

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,13 @@
 var mainApp = angular.module('ngApp', ['ngAnimate', 'ui.bootstrap','ngMap']);
+//FoodService-----------------------------------------------------------------------------
 mainApp.factory('FoodService', function(){
   var foodService = {
     food: [
-    {imageLink:"images/Sichuan.jpg",name:"LaoSiChuan",address:"Chinese Cultural Center, 668 N 44th St #108, Phoenix, AZ 85008"},
-    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
-    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
-    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
-    {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
+    {id:"0", imageLink:"images/SzechwanPalace.jpg",name:"Szechwan Palace", cname:"老四川", address:"Chinese Cultural Center, 668 N 44th St #108, Phoenix, AZ 85008", phone:"(602)685-0888", website:"http://www.szechwanpalacechinese.com/", style:"Szechwan style"},
+    {id:"1", imageLink:"images/PacificSeafoodBuffet.jpeg",name:"Pacific Seafood Buffet", cname:"太平洋海鲜自助", address:"8172 W Bell Rd, Glendale, AZ 85308", phone:"(623)776-7888", website:"http://www.pacificseafoodbuffet.com/", style:"Buffet"},
+    {id:"2", imageLink:"images/ThaiBasil.jpg",name:"Thai Basil",address:"1111 S Rural Rd, Tempe, AZ 85281", phone:"(480)557-0101", website:"http://www.thaibasilasu.com/", style:"Thai food" },
+    {id:"3", imageLink:"images/PhoenixPalace.jpg",name:"Phoenix Palace",address:"Dobson Park Plaza, 2075 N Dobson Rd, Chandler, AZ 85224", phone:"(480)855-4047", website:"http://www.phoenixpalacechandleraz.com/", style:"Cantonese style"},
+    {id:"4", imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
     {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
     {imageLink:"images/restaurant1.jpg",name:"1",address:"1"},
     {imageLink:"images/restaurant1.jpg",name:"1",address:"1"}
@@ -15,7 +16,7 @@ mainApp.factory('FoodService', function(){
   };
   return foodService;
 });
-
+//Carousel Controller------------------------------------------------------------------------
 mainApp.controller('CarouselCtrl', function ($scope) {  
   $scope.myInterval = 5000;
   $scope.noWrapSlides = false;
@@ -40,27 +41,27 @@ mainApp.controller('CarouselCtrl', function ($scope) {
     }
   ];   
 });
-
+//Tab Controller-----------------------------------------------------------------------------
 mainApp.controller('TabsCtrl',function ($scope, $timeout, NgMap, FoodService){
   $scope.food = FoodService.food;
   $scope.rate = 7;
   $scope.max = 10;
   $scope.isReadonly = false;
-  
+  //rating
   $scope.hoveringOver = function(value) {    
     $scope.overStar = value;
     $scope.percent = 100 * (value / $scope.max);
   };
   
   
-
+//google map
    $scope.maps = {};  
 
         NgMap.getMap({ id: 'map1' }).then(function (map) {
                 map.showInfoWindow('bar1', 'marker1'); //show marker on map load
                 $scope.maps['map1'] = map;
         });
-
+//restaurant
   $scope.myVar = true;
   $scope.myVarFunc = function(food){
 	  $scope.myVar = !$scope.myVar;  
@@ -73,7 +74,15 @@ mainApp.controller('TabsCtrl',function ($scope, $timeout, NgMap, FoodService){
                 });
   };
 
-
-     
-  
+  //show restaurant infomation
+  $scope.oneAtTime = true;
+  var restaurantPage = "html/restaurantInfo.html"; 
+  $scope.restaurantInfo = function(restaurantName){
+    restaurantPage = "html/" + restaurantName + ".html"; 
+    console.log(restaurantPage);
+  };
+  $scope.showRestaurantPage = function(){
+    return restaurantPage;
+  }
+ 
 });
